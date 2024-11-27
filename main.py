d3948d7902f57bc32a9509758262ae8cb47f3faf
xiiomara19 <xiomaracaceres19.07@gmail.com>
2024-11-27 12:07:32 +0100

    Asignación inicial de trabajadores en main.py (reorganización)

--- main.py
+++ main.py
@@ -1,8 +1,71 @@
 import readTables
 
+######### DATOS INICIALES #########
+# Ruta del archivo de Excel
+archivo = 'DATOS turnos HB compartir.xlsm'
+
+# Leer el excel y asígnarlo a variables globales
+(trabajadores_por_equipo, array_id_trabajadores, cantidad_trabajadores, array_puestos_de_trabajo, cantidad_puestos, matriz_ILUO, matriz_Prioridades, array_Maq_Prio, array_OP_Maq) = readTables.getTablesInfo(archivo)
+
+
+######### ASIGNACIÓN INICIAL #########
+# Implementar una primera función que asigne trabajadores con mayor experiencia 
+# (niveles 3 y 4 en la matriz ILUO) a los puestos prioritarios. 
+# Los trabajadores restantes serán distribuidos usando un enfoque como el hill climbing.
+
+def repartoTrabajadoresExperimentadosPrioridadConocimiento(prioridad, conocimiento, possibleSolution, cantidad_trabajadores, cantidad_puestos,array_trabajadores_disponibles, matriz_Prioridades, matriz_ILUO):
+    """
+    Asigna a los trabajadores con mayor conocimiento (ILUO 4 y 3) a los puestos principales.
+    La asignación se realiza considerando las prioridades definidas en la matriz_Prioridades.
+    """
+    
+    # Recorrer cada trabajador
+    for trabajador_j in range(len(possibleSolution)):
+        # print(" Id de trabajador actual = ", trabajador_j)
+        
+        # Verificar si el trabajador está disponible
+        esteTrabajadorEstaDisponible = array_trabajadores_disponibles[trabajador_j]
+        # print("el trabajador j está disponible = ", esteTrabajadorEstaDisponible )
+
+        # Recorrer cada puesto disponible
+        for puesto_i in range(cantidad_puestos):
+            # print(" Id de puesto actual = ", puesto_i)
+
+            #Verificar si este puesto no está inicialmente ocupado
+            if puesto_i in possibleSolution:
+                #Continuar con el siguiente puesto
+                continue
+            
+            # Consultar la prioridad del trabajador para este puesto
+            prioridadTrabajador_j_enPuesto_i = matriz_Prioridades[trabajador_j][puesto_i]
+            # print("prioridadTrabajador_j_enPuesto_i = ", prioridadTrabajador_j_enPuesto_i)
+            
+            # Consultar el nivel de experiencia (ILUO) del trabajador en este puesto
+            ILUOTrabajador_j_enPuesto_i = matriz_ILUO[trabajador_j][puesto_i]
+            # print("ILUOTrabajador_j_enPuesto_i = ", ILUOTrabajador_j_enPuesto_i)
+           
+            # Condiciones para asignar el trabajador al puesto:
+            # 1. El trabajador debe estar disponible.
+            # 2. El puesto debe ser prioritario para el trabajador (prioridad = 1).
+            # 3. El trabajador debe tener nivel de conocimiento ILUO de 4 o 3 para el puesto.
+            # 4. El puesto no debe estar ya ocupado en la solución propuesta.
+            if ( esteTrabajadorEstaDisponible == True and prioridadTrabajador_j_enPuesto_i == prioridad and (ILUOTrabajador_j_enPuesto_i == conocimiento)):
+                # print("Este puesto ", puesto_i, "está vacío, se lo añado a j", trabajador_j )
+                possibleSolution[trabajador_j] = puesto_i
+                # print("De momento possibleSolution va así: ", possibleSolution)
+    
+    #Devolver la lista actualizada
+    return possibleSolution
+
+def repartoTrabajadoresExperimentados(possibleSolution, cantidad_trabajadores, cantidad_puestos,array_trabajadores_disponibles, matriz_Prioridades, matriz_ILUO):
+    for prio in range(1, 10):  # Itera de 1 a 9 --> Prioridades de Prio_Maq
+        for cono in range(4, 0, -1):  # Itera de 4 a 1 --> Conocimientos de ILUO
+            possibleSolutionNew = repartoTrabajadoresExperimentadosPrioridadConocimiento(prio, cono, possibleSolution, cantidad_trabajadores, cantidad_puestos, array_trabajadores_disponibles, matriz_Prioridades, matriz_ILUO)
+            print("Possible solution after workers with experience (priority = ", prio  ,") (ILUO = ", cono, "): ", possibleSolutionNew)
+    return possibleSolutionNew
+
 
 ######### FUNCIÓN OBJETIVO #########
-
 def funcionObjetivo(possibleSolution):
     """
     Calcula la puntuación total de la asignación, maximizando el número de máquinas operativas y
@@ -43,11 +106,10 @@ def funcionObjetivo(possibleSolution):
     return puntuacion_total
 
 
-######### MAIN #########
-# Ruta del archivo de Excel
-archivo = 'DATOS turnos HB compartir.xlsm'
 
-# Leer el excel y asígnarlo a variables globales
-(trabajadores_por_equipo, array_id_trabajadores, cantidad_trabajadores, array_puestos_de_trabajo, cantidad_puestos, matriz_ILUO, matriz_Prioridades, array_Maq_Prio, array_OP_Maq) = readTables.getTablesInfo(archivo)
+######### INSERT #########
+
+
+######### HILL CLIMBING #########
 
 
