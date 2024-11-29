5bf477e8bc852f7192b7979180339d771089e85b
iraidabadlopez <iraidabadlopez@gmail.com>
2024-11-29 15:46:58 +0100

    Commit innecesario

---
+++ app.py
@@ -0,0 +1,35 @@
+import readTables
+import main
+
+######### DATOS INICIALES #########
+# Ruta del archivo de Excel
+archivo = 'DATOS turnos HB compartir.xlsm'
+
+# Leer el excel y asígnarlo a variables globales
+# Estas variables incluyen información sobre los trabajadores, los puestos disponibles, y matrices que definen prioridades y niveles de experiencia (ILUO).
+(trabajadores_por_equipo, array_id_trabajadores, cantidad_trabajadores, array_puestos_de_trabajo, cantidad_puestos, matriz_ILUO, matriz_Prioridades, array_Maq_Prio, array_OP_Maq) = readTables.getTablesInfo(archivo)
+
+
+def main_program():
+    # Solicitar al usuario el equipo al que pertenece su equipo con control de errores
+    intentos = 0
+    while intentos < 4:
+        equipo_usuario = input("Introduce el equipo de tu preferencia (A, B, C, D, E): ").strip().upper()
+        # Verificar si el equipo ingresado es válido
+        if equipo_usuario in ['A', 'B', 'C', 'D', 'E']:
+            # Obtener los valores asignados por el equipo
+            array_trabajadores_disponibles = readTables.asignar_valores_por_equipo(trabajadores_por_equipo, equipo_usuario, cantidad_trabajadores, array_id_trabajadores)
+        
+            # Mostrar el resultado
+            print(array_trabajadores_disponibles)
+            #Salir del while
+            break
+        else:
+            print("Error: Entrada inválida. Por favor, ingrese uno de los equipos válidos: A, B, C, D, E.")
+            intentos += 1
+
+    if intentos == 4:
+        print("Ha superado el número de intentos fallidos. Vuelva a intentarlo más tarde.")
+
+if __name__ == "__main__":
+    main_program()
